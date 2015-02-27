d80862047b03887de0a0ff4a3c1b80ccaf5322c7
Magne Vikjord <mvikjord@gmail.com>
2015-02-27 16:09:00 +0100

    Initialize auctions by a fair dice roll

--- negotiating-agents/src/agents/tradeAgent.java
+++ negotiating-agents/src/agents/tradeAgent.java
@@ -1,8 +1,12 @@
 package agents;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
+import jade.content.schema.facets.DocumentationFacet;
+import jade.core.behaviours.SimpleBehaviour;
 import tools.Inventory;
 import jade.core.AID;
 import jade.core.Agent;
@@ -19,62 +23,126 @@ import jade.lang.acl.MessageTemplate;
 @SuppressWarnings("serial")
 public class TradeAgent extends Agent {
 
-  private final static String serviceType = "trader-agent";
+  private final static String SERVICE_TYPE = "trader-agent";
+  private final static int AGENTS_BEFORE_START = 3;
+
+  private final static Random rng = new Random(System.currentTimeMillis());
 
 	private ArrayList<String> items = new ArrayList<String>(11);
 	private ArrayList<String> wantedItems = new ArrayList<String>(3);
 	private ArrayList<AID> traderAgents = new ArrayList<AID>();
 	private ArrayList<AID> contactList = new ArrayList<AID>();
-	private int cleared = 0; // TODO: better name
 	private int gold = 100;
+
+  // Gambling for chance to auction
+  private int currentGamblingRound = 1;
+  private double myRoll;
+  private int remainingRolls;
 	
-protected void setup(){	
-		
-		//You have to say this in your head like the StarCraft unit
-		System.out.println(this.getLocalName()+" agent online.");	
-		register();
-		
-		//Get items and wantedItems
-		addBehaviour(new RequestInventoryAndWantedItems());
-		
-		//Waits until told to start trade process
-		addBehaviour(new GoCode());
-				
-		//Wait for item checks
-		addBehaviour(new CheckForItem());
-				
-		//Wait for Trade acceptances
-		addBehaviour(new Trading());
-	}
+protected void setup(){
+  //You have to say this in your head like the StarCraft unit
+  System.out.println(this.getLocalName()+" agent online.");
+  register();
+
+  //Get items and wantedItems
+  addBehaviour(new RequestInventoryAndWantedItems());
+
+  //Wait for item checks
+  addBehaviour(new CheckForItem());
+
+  //Wait for Trade acceptances
+  addBehaviour(new Trading());
+
+  // Start the gambling round if all traders are present
+  tryInitializeGamblingRound();
+
+  // Answer requests to gamble for a chance at the auction
+  addBehaviour(new GambleForAuctionChance());
+}
 
 /**
- * Inner class GoCode
- * This class allows the trade agents to wait with trading until
- * told to start. This is to allow all the trader agents to get
- * their inventories and register with DF service so we don't miss
- * any.
+ * Checks if AGENTS_BEFORE_START agents are present, and initializes the
+ * trading round if they are.
  */
-private class GoCode extends CyclicBehaviour {
-	public void action() {
-		MessageTemplate mt = MessageTemplate.MatchPerformative(ACLMessage.CFP); // TODO: probably wrong performative, inform is better
-		ACLMessage msg = myAgent.receive(mt);
-		
-		if(msg != null){
-			cleared++;
-			if(cleared > 1 && contactList.isEmpty()){
-				System.out.println("System is done trading!");
-				block();
-				doDelete();
-			}
-			System.out.println(myAgent.getLocalName()+" asked to start trading.");
-			addBehaviour(new GetAgentList());
-			
-			addBehaviour(new StartTrading());
-		}
-		else{
-			block();
-		}
-	}
+protected void tryInitializeGamblingRound() {
+  AID[] agents = getAgentList();
+  if (agents.length < AGENTS_BEFORE_START) {
+    return;
+  }
+
+  ACLMessage requestToGamble = new ACLMessage(ACLMessage.REQUEST);
+  requestToGamble.setConversationId("gambling-round-" + currentGamblingRound);
+
+  // Important note, we actually send this message to ourselves too
+  // because it makes the code simpler.
+  for (AID agent : agents) {
+    requestToGamble.addReceiver(agent);
+  }
+}
+
+/**
+ * Rolls a dice (double 0-1) and sends the results to all other traders
+ */
+private class GambleForAuctionChance extends CyclicBehaviour {
+  public void action() {
+    MessageTemplate mt = MessageTemplate.and(
+      MessageTemplate.MatchPerformative(ACLMessage.REQUEST),
+      MessageTemplate.MatchConversationId("gambling-round-" + currentGamblingRound)
+    );
+    ACLMessage msg = myAgent.receive(mt);
+
+    if(msg != null) {
+      myRoll = rng.nextDouble();
+      System.out.println(myAgent.getLocalName() + " rolled a " + myRoll);
+
+      ACLMessage reply = new ACLMessage(ACLMessage.INFORM);
+      reply.setConversationId("gambling-round-" + currentGamblingRound);
+      reply.setContent(Double.toString(myRoll));
+      AID[] agentList = getAgentList();
+      for (AID agent : agentList) {
+        if (agent.equals(myAgent.getAID())) continue;
+        reply.addReceiver(agent);
+      }
+
+      myAgent.send(reply);
+      remainingRolls = agentList.length - 1;
+      myAgent.addBehaviour(new ReceiveGambleRolls());
+
+    } else {
+      block();
+    }
+  }
+}
+
+/**
+ * Receive dice rolls from other traders, and once all rolls are in,
+ * checks if this agent is the winner, and starts trading if it is.
+ */
+private class ReceiveGambleRolls extends CyclicBehaviour {
+  private final List<Double> rolls = new ArrayList<>();
+  public void action() {
+    MessageTemplate mt = MessageTemplate.and(
+      MessageTemplate.MatchPerformative(ACLMessage.INFORM),
+      MessageTemplate.MatchConversationId("gambling-round-" + currentGamblingRound)
+    );
+    ACLMessage msg = myAgent.receive(mt);
+    if (msg != null) {
+      rolls.add(Double.parseDouble(msg.getContent()));
+      remainingRolls--;
+
+      if (remainingRolls == 0) {
+        myAgent.removeBehaviour(this);
+        currentGamblingRound++;
+
+        if (myRoll > Collections.max(rolls)) {
+          System.out.println(myAgent.getLocalName() + " is the winner if the gambling round");
+          myAgent.addBehaviour(new StartTrading());
+        }
+      }
+    } else {
+      block();
+    }
+  }
 }
 
 /**
@@ -227,6 +295,9 @@ private class StartTrading extends Behaviour {
 			step = 0;
 			rej = 0;
 			step = 0;
+
+      tryInitializeGamblingRound();
+
 			return false;
 		}
 		return false;
@@ -295,6 +366,7 @@ private class Trading extends CyclicBehaviour {
  * Retrieves other available trader agents from DF agent
  * and adds them to traderAgents list.
  */
+@Deprecated
 private class GetAgentList extends OneShotBehaviour {
 	public void action() {
 	if(traderAgents.isEmpty()){
@@ -302,7 +374,7 @@ private class GetAgentList extends OneShotBehaviour {
 		
 		DFAgentDescription template = new DFAgentDescription();
 		ServiceDescription sd = new ServiceDescription();
-		sd.setType(serviceType);
+		sd.setType(SERVICE_TYPE);
 		template.addServices(sd);
 		
 		try{
@@ -331,6 +403,28 @@ private class GetAgentList extends OneShotBehaviour {
 } 	
 }// End of inner class getAgentList
 
+protected AID[] getAgentList() {
+  AID[] agents;
+
+  DFAgentDescription template = new DFAgentDescription();
+  ServiceDescription sd = new ServiceDescription();
+  sd.setType(SERVICE_TYPE);
+  template.addServices(sd);
+
+  try {
+    DFAgentDescription[] result = DFService.search(this, template);
+    agents = new AID[result.length];
+    for (int i = 0; i < result.length; i++) {
+      agents[i] = result[i].getName();
+    }
+    return agents;
+  }
+  catch (FIPAException fe) {
+    fe.printStackTrace();
+    return null;
+  }
+}
+
 /*
  * DF register method
  */
@@ -339,7 +433,7 @@ protected void register() {
 	DFAgentDescription dfd = new DFAgentDescription(); 
 	dfd.setName(getAID()); 
 	ServiceDescription sd = new ServiceDescription(); 
-	sd.setType(serviceType);
+	sd.setType(SERVICE_TYPE);
 	sd.setName("TraderAgent" + System.currentTimeMillis()); //Gives each agent unique name
 	dfd.addServices(sd); 
 	try { 
